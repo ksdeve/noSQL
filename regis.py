0bfc8e1aa3f3806418165d2709ce5f732b7947ee
ksdev <salmon.kevin.pro@gmail.com>
2025-02-04 12:40:14 +0100

    starting python

---
+++ regis.py
@@ -0,0 +1,18 @@
+import redis
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+r.set('user:1:name', 'John Doe')
+r.set('user:1:email', 'john.doe@example.com')
+
+r.setex('session_key', 3600, 'session_data')
+
+user_name = r.get('user:1:name')
+user_email = r.get('user:1:email')
+
+keys = ['user:1:name', 'user:1:email']
+values = r.mget(keys)
+# Convert byte values to strings
+values = [value.decode('utf-8') for value in values]
+
+
